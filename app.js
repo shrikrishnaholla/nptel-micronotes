681daf264a7c2eb3d9ae0b345b51605c95485c39
Srinidhi J <srinidhij.21@gmail.com>
2014-02-02 15:01:08 +0530

    Added route for posting user login

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/create_account', routes.create_account);
-//app.post('/send_login', routes.send_login);
+app.post('/send_login', routes.send_login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
