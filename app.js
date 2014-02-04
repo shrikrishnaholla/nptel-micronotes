ebd590ddcc6f8ec323a876efa472bd63d347a0f7
Srinidhi J <srinidhij.21@gmail.com>
2014-02-04 05:22:35 +0000

    Added route for login

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/create_account', routes.create_account);
 app.post('/send_login', routes.send_login);
+app.post('/login', routes.login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
